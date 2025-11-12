58d4ad734efc75d43bbc6426751a3c606cdb665b
vivek3401 <vivek875474@gmail.com>
2025-11-12 21:41:12 +0530

    Add files via upload

---
+++ Medical-management-system-main (1)/Medical-management-system-main/src/com/project/Main.java
@@ -0,0 +1,17 @@
+package com.project;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Tom Marvolo Riddle
+ */
+
+public class Main
+{
+    public static void main(String args[]) 
+    {
+       new Welcome();
+    }
+}
